f41028e94ff80fa5c91d0006d8111d3eb1b67021
masalaza-cn <83977863+masalaza-cn@users.noreply.github.com>
2022-09-12 15:06:31 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+//html selector variables
+let background = document.querySelector(".background");
+let pink = document.querySelector(".pink");
+let green = document.querySelector(".green");
+let blue = document.querySelector(".blue");
+let lightGreen = document.querySelector(".light-green");
+let circle = document.querySelector(".color");
+
+
+//update the css styling
+// task 1:  set the background selector to a background color of black.
+
+
+// task 2: set the pink selector to a background color of pink.
+
+
+// task 3: set the green selector to a background color of green.
+
+
+// task 4: set the blue selector to a background color of blue.
+
+
+
+// task 5: set the lightGreen selector to a background color of lightGreen.
+
+
+// task 6: set the row1 selector to have a font color of white.
+
+
+// task 7: using the pink selector, make the pink circle disappear! What css property and value do we use for this???
